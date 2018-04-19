69741c239b1472caad544e8a701b6b1be0b48224
jsjackson263 <jsjackson263@users.noreply.github.com>
2018-04-19 22:52:51 +1200

    initial commit

---
+++ src/main/java/org/example/Spring4ActuatorApplication.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring4ActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring4ActuatorApplication.class, args);
+	}
+}
